6c8312be6138a7ca8248e5e096d5d8d348da2c78
Nitka04 <nuta0403@ukr.net>
2023-03-05 13:33:18 +0200

    05.03  string comand and function

---
+++ src/Lessons_Topic/Main.java
@@ -0,0 +1,33 @@
+package Lessons_Topic;
+
+public class Main {
+    public static void main(String[] args) {
+        /* byte a =100;
+        a = (byte) (a*2);
+        System.out.println(a); */
+
+       /* double b =63;
+        double c=13;
+        double d= b/c;
+        System.out.println(d); */
+
+       /* double b =63;
+        double c=13;
+        int d= (int) (b/c);
+        System.out.println(d); */
+
+       /* char ch = 'A';
+        System.out.println(ch);
+
+        char ch1 = 111;
+        System.out.println(ch1);
+
+        int xy = 12;
+        System.out.println(xy+ch1); */
+
+       /* int i = 100;
+        char ch= 'z';
+        System.out.println(i+ch);
+        System.out.println(i+ch); */
+    }
+}
---
+++ src/Lessons_Topic/StringMethods.java
@@ -0,0 +1,96 @@
+package Lessons_Topic;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        // valuof -возвращае  строковое представление значение примитивніх типов
+        //
+        /*  int w =3;
+        System.out.println(String.valueOf(w));
+        System.out.println(w);
+
+        //print -не переносит на новую строку
+        System.out.print(w);
+        System.out.print(w); */
+
+        // символі єкранирования \n - перенос; \", \', \b/
+
+        /*System.out.println("я тестирую \b как боженька");
+        int a =12;
+        int b =32;
+        int c= 33;
+        System.out.println (a+","+b+","+c+"."); */
+
+        // все методі клаSystem.out.printlnсса стринг не изменяют строку, а возвращают ссілку на новій обїет
+        //  concat  производит слияние строк
+
+        /* String string="I love testing very much";
+        String string2=string.concat(" today");
+        String string3=string+" today";
+        System.out.println(string);
+        System.out.println(string2);
+        System.out.println(string3); */
+
+        // сharAt - возвращает символ из строки по указанному символу
+        // первій символ будет "0", java считает с 0
+        String string="I love testing very much";
+        String string2="";
+        char ch =string.charAt(1);
+        System.out.println(" сharAt"+ch);
+
+        // contains
+
+        // length - возвращает уоличесство символов строки
+       int x= string.length();
+        System.out.println("length="+x);
+        // isEmpty -если строка пустая возвращает истинуб если нет лож
+         boolean result=string.isEmpty();
+        System.out.println("isEmpty="+result);
+        //starsWith- возвращает истину если указанній символ или строк
+        // совпадают с началом строки
+        //endsWith- аналогичтно,starsWith только наоборот
+        boolean result2=string.startsWith("I love");
+        boolean result3= string.endsWith("h");
+        System.out.println(ch);
+        System.out.println("startsWith="+ result2);
+        System.out.println("endsWith="+result3);
+
+// toLowerCase - возвращает строку в нижнем регистре
+// toUpperCase- возвращает строку в верхнем решистре
+        System.out.println("toLowerCase="+string.toLowerCase());          System.out.println(string);
+        System.out.println("toUpperCase="+string.toUpperCase());
+        // indexOf -ищет указанній символ начиная с начала строки
+        // возвращает индекс первого найденого по порядку совпадающего символа
+        // если совпадений нет , возвращает -1
+        //  lastIndexOf ищет с конца
+        int index=string.indexOf('w');
+        System.out.println("indexOf="+index);
+        //substring - вірезает указанній промежуток символов
+        // возвращает новую строку
+        String string3 =string.substring(7,20);
+        System.out.println("substring="+string3);
+        // replace- вовращает строку , заменяя указанніе символі и строки
+        String string4 =string.replace("ch","ch because");
+        System.out.println("replace="+string4);
+        //split  разбивает на массив строк
+String  i=string.split(" ")[0] ;
+String love =string.split(" ")[1];
+String testing =string.split(" ")[2];
+        System.out.println("split="+i);
+        System.out.println(love);
+        System.out.println(testing);
+        // trim убирает начальніе и конечніе пробелі
+        System.out.println(string);
+        System.out.println("trim="+string.trim());
+
+        String String_1 = "I am testing like a god";
+        String String_2 = " the best of the best.";
+        char ch1=String_1.charAt(6);
+        String ch2=String.valueOf(ch1);
+        int index1=String_1.indexOf('e');
+        boolean result_2=String_2.contains(ch2);
+        System.out.println("index1="+index1);
+        System.out.println("Строка\" "+String_2+ "содержит в себе символ "+ch+"и єто "+result_2+
+                " символ 'е' находиться в строке под номером "+index1);
+
+    }
+}
